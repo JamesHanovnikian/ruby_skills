39026640bbc82f534dc053cea8b884d37c0e402e
YOUR NAME <jhanov@gmail.com>
2021-10-22 07:33:03 -0500

    add morehashes exercises

--- changingdata.rb
+++ changingdata.rb
@@ -13,6 +13,16 @@
 # p "HEllo"
 # p numbers_hash
 
+make a variable for max, min
+have a count go up if max or min changes
+while loop 
+if statement 
+  add one to count for a change if it goes through  
+
+print everything out
+
+
+
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
 
---
+++ practice.rb
@@ -0,0 +1,20 @@
+# Given an array of strings, return a hash that provides of a count of how many times each string occurs.
+
+# Input: ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
+
+# Output: {"Dewey" => 6, "Truman" => 5}
+
+# Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
+
+array = ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
+
+i = 0
+word_frequencies = {}
+while i < array.length
+  if word_frequencies[array[i]] == nil
+    word_frequencies[array[i]] = 0
+  end
+  word_frequencies[array[i]] += 1
+  i += 1
+end
+p word_frequencies
